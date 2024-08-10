f81561c8e6ca3027bd6fb2a4bb0e35db3a9587e1
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-08-10 17:24:36 +0530

    Fix html css js logic for appending,styling and structuring of book container

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-library
+
+
+https://unsplash.com/photos/white-book-page-on-brown-wooden-table-X-sxo5qHbyw by  https://unsplash.com/@elimendeinagella
--- app.js
+++ app.js
@@ -4,14 +4,69 @@ let body = document.querySelector("body");
 let popUp = document.querySelector("#pop-up");
 let navbar = document.querySelector(".navbar");
 let closePopUpButton = document.querySelector("#close-pop-up-button");
+let popUpSubmitButton = document.querySelector("#pop-up-submit-button");
+// let bookContainer;
+
+
+const myLibrary = [
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        noOfPages: 281,
+        noOfStars: 5,
+        isBookRead: true
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        noOfPages: 328,
+        noOfStars: 4,
+        isBookRead: false
+    },]
 
 
 
+function createNewBook(title, author, noOfPages,noOfStars,isBookRead) {
+    this.title = title;
+    this.author = author;
+    this.noOfPages = noOfPages;
+    this.noOfStars = noOfStars;
+    this.isBookRead = isBookRead;
+   
+};
+
+
+function addBookToLibrary() {
+    // do stuff here
+    myLibrary.forEach(book => {
+        
+        
+    });
+
+
+  }
+
+
+
+
+
+
+
+  function removeLandingPageElements() {
+    landingSection.remove();
+
+}
 
 
 function blurBodyOpacity() {
     landingSection.style.opacity = 0.1;
     navbar.style.opacity = 0.1;
+    
+}
+
+function dontBlurBodyOpacity() {
+    landingSection.style.opacity = 1;
+    navbar.style.opacity = 1;
     
 }
 
@@ -37,3 +92,25 @@ closePopUpButton.addEventListener("click", ()=> {
     landingSection.style.opacity = 1;
     navbar.style.opacity = 1;
 })
+
+
+
+popUpSubmitButton.addEventListener("click", ()=> {
+    event.preventDefault();
+    removeLandingPageElements()
+    addBookToLibrary();
+    popUp.style.visibility = "hidden";
+    dontBlurBodyOpacity();
+
+    let bookContainer = document.createElement("div");
+    bookContainer.style.cssText = "width:80vw; height:80vh; border:2px solid black; background-color:skyblue; margin:auto;";
+    
+    body.appendChild(bookContainer); 
+    
+
+
+    
+})
+
+
+
