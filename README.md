7f1c51db830bc99d40697f3abd25aae676f11fd6
abhilash15500 <89824888+abhilash15500@users.noreply.github.com>
2024-08-11 19:24:26 +0530

    Implement js logic to toggle read/unread book

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 
 https://unsplash.com/photos/white-book-page-on-brown-wooden-table-X-sxo5qHbyw by  https://unsplash.com/@elimendeinagella
+
+
+
+https://www.freepik.com/free-vector/red-text-book-closed-icon_70015834.htm#fromView=search&page=1&position=3&uuid=fdfe9957-c39b-4d2a-a8f6-89d0e3010ed1 by https://www.freepik.com/author/jemastock
+
+
--- app.js
+++ app.js
@@ -5,46 +5,151 @@ let popUp = document.querySelector("#pop-up");
 let navbar = document.querySelector(".navbar");
 let closePopUpButton = document.querySelector("#close-pop-up-button");
 let popUpSubmitButton = document.querySelector("#pop-up-submit-button");
-// let bookContainer;
+let arrayIndexNo = 0;
 
-
-const myLibrary = [
-    {
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        noOfPages: 281,
-        noOfStars: 5,
-        isBookRead: true
-    },
-    {
-        title: "1984",
-        author: "George Orwell",
-        noOfPages: 328,
-        noOfStars: 4,
-        isBookRead: false
-    },]
+let bookishLogo = document.querySelector("#bookish-logo");
 
 
 
-function createNewBook(title, author, noOfPages,noOfStars,isBookRead) {
+bookishLogo.addEventListener("click",()=>{
+    window.location.reload()
+});
+
+
+
+
+
+
+
+
+
+
+const myLibrary = [];
+
+
+
+
+
+function createNewBook(title, author, noOfPages, noOfStars, isBookRead) {
     this.title = title;
     this.author = author;
     this.noOfPages = noOfPages;
     this.noOfStars = noOfStars;
     this.isBookRead = isBookRead;
-   
+
 };
+
 
 
 function addBookToLibrary() {
     // do stuff here
+
+    let newObject = new createNewBook(title.value, author.value, pages.value, stars.value, read.value);
+    myLibrary.push(newObject);
+
+
+    let bookContainer = document.createElement("div");
+    bookContainer.style.cssText = "width:80vw;margin:auto;";
+
+    body.appendChild(bookContainer);
+
+
     myLibrary.forEach(book => {
+        bookContainer.innerHTML = "";
+        let createBookDiv = document.createElement("div");
+        createBookDiv.classList.add("book-div");
+
+        // arrayIndexNo = createBookDiv.dataset.indexNumber - 1;
+        arrayIndexNo = myLibrary.length-1;
         
+        createBookDiv.setAttribute("data-index-number", `${arrayIndexNo}`);
+
+
+
+        let bookTitle = document.createElement('p');
+        bookTitle.classList.add("book-title");
+        let bookAuthor = document.createElement('p');
+        let bookPages = document.createElement('p');
+        let bookStars = document.createElement('p');
+
+        let bookIsRead = document.createElement('button');
+        bookIsRead.classList.add("book-isread-button");
+
+        let deleteBook = document.createElement("button");
+        deleteBook.classList.add("delete-book")
+
+        let createButtonDiv = document.createElement("div");
+        createButtonDiv.classList.add("button-div");
+
+
+
+
+
+        bookContainer.appendChild(createBookDiv);
+
+        createBookDiv.appendChild(bookTitle);
+        createBookDiv.appendChild(bookAuthor);
+        createBookDiv.appendChild(bookPages);
+        createBookDiv.appendChild(bookStars);
+        createBookDiv.appendChild(createButtonDiv);
+        createButtonDiv.appendChild(bookIsRead);
+        createButtonDiv.appendChild(deleteBook);
+
+
+
+        bookTitle.textContent = title.value;
+        bookAuthor.textContent = `by - ${author.value}`;
+        bookPages.textContent = `${pages.value} Pages`;
+        bookStars.textContent = `${stars.value}⭐`;
+        bookIsRead.textContent = `READ`;
+        deleteBook.textContent = "DELETE";
+
+        bookTitle.style.cssText = "font-weight:800; font-size:25px;text-transform:uppercase;";
+
+
+
+        deleteBook.addEventListener("click", () => {
+            createBookDiv.remove();
+            arrayIndexNo = Number(createBookDiv.dataset.indexNumber);
+            // myLibrary.splice(arrayIndexNo, 1);
+            myLibrary[arrayIndexNo] = null;
+            
+
+
+
+        })
+
+      
         
+        bookIsRead.addEventListener("click", () => {
+            
+            let parentContainer = bookIsRead.parentElement;
+            let grandParentContainer = parentContainer.parentElement;
+           
+
+            if(myLibrary[grandParentContainer.dataset.indexNumber].isBookRead === "on"){
+
+       
+            myLibrary[grandParentContainer.dataset.indexNumber].isBookRead = "off";  
+            bookIsRead.textContent = "NOT READ";  
+            bookIsRead.style.backgroundColor = "blue";
+            bookIsRead.style.border = "2px solid blue";  
+        }
+        else if (myLibrary[grandParentContainer.dataset.indexNumber].isBookRead === "off") {
+            myLibrary[grandParentContainer.dataset.indexNumber].isBookRead = "on";
+            bookIsRead.textContent = "READ";
+            bookIsRead.style.backgroundColor = "green";
+            bookIsRead.style.border = "2px solid green";         
+
+        }
+            
+        })
+
+     
     });
 
 
-  }
+}
 
 
 
@@ -52,7 +157,10 @@ function addBookToLibrary() {
 
 
 
-  function removeLandingPageElements() {
+
+
+
+function removeLandingPageElements() {
     landingSection.remove();
 
 }
@@ -61,13 +169,15 @@ function addBookToLibrary() {
 function blurBodyOpacity() {
     landingSection.style.opacity = 0.1;
     navbar.style.opacity = 0.1;
-    
+
+
 }
 
 function dontBlurBodyOpacity() {
     landingSection.style.opacity = 1;
     navbar.style.opacity = 1;
-    
+
+
 }
 
 function displayPopUp() {
@@ -78,8 +188,11 @@ function displayPopUp() {
 
 
 
+
+
+
 landingPageButtons.forEach(button => {
-    button.addEventListener("click", ()=>{
+    button.addEventListener("click", () => {
         blurBodyOpacity();
         displayPopUp();
 
@@ -87,7 +200,7 @@ landingPageButtons.forEach(button => {
 })
 
 
-closePopUpButton.addEventListener("click", ()=> {
+closePopUpButton.addEventListener("click", () => {
     popUp.style.visibility = "hidden";
     landingSection.style.opacity = 1;
     navbar.style.opacity = 1;
@@ -95,21 +208,20 @@ closePopUpButton.addEventListener("click", ()=> {
 
 
 
-popUpSubmitButton.addEventListener("click", ()=> {
-    event.preventDefault();
+popUpSubmitButton.addEventListener("click", () => {
+
+    
     removeLandingPageElements()
-    addBookToLibrary();
-    popUp.style.visibility = "hidden";
+
+
     dontBlurBodyOpacity();
 
-    let bookContainer = document.createElement("div");
-    bookContainer.style.cssText = "width:80vw; height:80vh; border:2px solid black; background-color:skyblue; margin:auto;";
-    
-    body.appendChild(bookContainer); 
-    
+    popUp.style.visibility = "hidden";
+
+    addBookToLibrary();
+    event.preventDefault();
 
 
-    
 })
 
 
